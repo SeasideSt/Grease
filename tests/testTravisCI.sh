fa796e58512737c77c524666dab770c3122fabef
Johan Brichau <johan@inceptive.be>
2014-09-06 15:42:23 +0200

    update travis test script to load GLASS1 properly

--- tests/testTravisCI.sh
+++ tests/testTravisCI.sh
@@ -35,14 +35,30 @@ OUTPUT_PATH="${PROJECT_HOME}/tests/travisCI.st"
 
 cat - >> $OUTPUT_PATH << EOF
 (Smalltalk includesKey: #UserGlobals) ifTrue:[
- "Load latest GLASS1 when on Gemstone"
- [ Metacello new
-    baseline: 'GLASS1';
-    repository: 'github://glassdb/glass:master/repository';
+"Upgrade GLASS to to 1.0-beta.9.3"
+Gofer new
+    url: 'http://seaside.gemtalksystems.com/ss/MetacelloRepository';
+    package: 'ConfigurationOfGLASS';
     load.
-  ] on: Warning
-    do:[:ex | Transcript show: ex greaseString. ex resume].
-].
+ConfigurationOfGLASS project updateProject.
+GsDeployer
+  deploy: [ (ConfigurationOfGLASS project version: '1.0-beta.9.3') load ].
+"Install GLASS from github"
+GsDeployer deploy: [
+ Metacello new
+  baseline: 'GLASS1';
+  repository: 'github://glassdb/glass:master/repository';
+  get.
+ Metacello new
+  baseline: 'GLASS1';
+  repository: 'github://glassdb/glass:master/repository';
+  onConflict: [ :ex | ex allow ];
+  onWarning: [ :ex | 
+        Transcript
+          cr;
+          show: ex description.
+        ex resume ];
+  load: 'default' ].
 
  Transcript cr; show: 'travis--->${OUTPUT_PATH}'.
  "Load the configuration or baseline"
